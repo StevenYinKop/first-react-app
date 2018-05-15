2ee6635cfcd69506c2a38781cbf2fe6bba356a7e
CinCommon <452495748@qq.com>
2018-05-15 20:34:12 +0800

    first redux demo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+
+import { createStore } from 'redux'
+// 通过reducer建立
+// 根据老的state和action 生成新的state
+function counter(state = 0, action) {
+    switch (action.type) {
+        case 'add':
+        return state + 1;
+        case 'remove':
+        return state - 1;
+        default:
+        return 10;
+    }
+}
+
+// 1. 新建store
+const store = createStore(counter)
+function listener() {
+    const current = store.getState()
+    console.log(current)
+}
+
+store.subscribe(listener)
+const init = store.getState()
+console.log(init)
+
+store.dispatch({type: 'add'})
+store.dispatch({type: 'add'})
+store.dispatch({type: 'add'})
+store.dispatch({type: 'add'})
